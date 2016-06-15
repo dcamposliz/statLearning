4dce105d27321aadadc0b35a43975ec7bfab9294
dcamposliz <dcampos.liz@gmail.com>
2016-06-15 12:05:36 -0700

    adding classification code

---
+++ ch4_classification.R
@@ -0,0 +1,24 @@
+#	-----------------------------------------------------------------------------------------------------------------------
+#
+#	Course: 		Statistical Learning; Stanford Online, Lagunita; (https://lagunita.stanford.edu/courses/HumanitiesandScience/StatLearning/Winter2015/about)
+#	Student: 		David Aaron Campos
+#	Repository:		https://github.com/dcamposliz/statLearning
+#	Date: 			June 2016
+#	Faculty: 		Trevor Hastie; Rob Tibshirani
+#	Textbook: 		Introduction to Statistical Learning; (http://www-bcf.usc.edu/~gareth/ISL/ISLR%20First%20Printing.pdf)
+#
+#	About R:		https://www.r-project.org/about.html
+#
+#	-----------------------------------------------------------------------------------------------------------------------
+#
+#	CHAPTER 4 - CLASSIFICATION
+#
+#	Index:
+#	
+#		
+#		
+#		
+#		
+
+#	-----------------------------------------------------------------------------------------------------------------------
+
